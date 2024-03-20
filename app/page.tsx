54b663c8fb2e5a64688c389d2d6861c9c748e57b
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2024-03-20 17:07:28 +0000

    fixed overflow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
-    <main className="flex justify-center ">
+    <main className="flex justify-center overflow-x-hidden">
       <Profile />
     </main>
   );
